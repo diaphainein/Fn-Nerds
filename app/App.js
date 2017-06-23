f50a4f5f0d989e88fa2fb074d3207d0d037a361e
adpopo <hxuanpham@gmail.com>
2017-06-22 20:10:26 -0700

    more setup stuff

--- app/App.js
+++ app/App.js
@@ -1,6 +1,9 @@
-// Importing ReactDOM and our routes
-import ReactDOM from "react-dom";
-import routes from "./config/routes";
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
 
-// Rendering our router to the "app" div in index.html
+// Grabs the Routes
+var routes = require("./config/routes");
+
+// Renders the contents according to the route page.
 ReactDOM.render(routes, document.getElementById("app"));
