bc65807557a55857420b9d447368461b560e947e
adpopo <hxuanpham@gmail.com>
2017-06-27 20:22:01 -0700

    layout stuff

--- app/components/children/Game.js
+++ app/components/children/Game.js
@@ -1,19 +1,41 @@
-// Include React
-var React = require("react");
-
 var Game = React.createClass({
 
   // Here we render the function
   render: function() {
+
     return (
-      <div className="container">
 
-  		{/* insert contents here */}
+			<div className="container">
+				{/* navbar */}
+					{/* logo far left */}
+					{/* menu button far right */}
+						{/* leave game (lobby) */}
+						{/* char sheet */}
+						{/* logout */}
 
-      </div>
-    );
+				{/* game div */}
+					{/* map 12*/}
+					<Map {/props/} />
+
+					{/* chat window 6*/}
+					<Chat {/props/} />
+					{/* char sheet 6*/}
+					<ChaSht {/props/} />
+
+					{/* turn indicator 3*/}
+					<button>{/* turn prop */}</button>
+					{/* dice roll modal button 3*/}
+					<button {/* diceroll */}>Roll Dice</button>
+					{/* glossary modal button 3*/}
+					<button {/* glossary */}>Glossary</button>
+					{/* end turn button 3*/}
+					<button {/* turn prop */}>End Turn</button>
+
+			</div>
+
+    	   );
   }
 });
 
-// Export the componen back for use in other files
+// Export the component back for use in other files
 module.exports = Game;
