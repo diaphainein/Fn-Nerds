499999a72660f1562130c0d40bb4db8c6e1ba6b4
adpopo <hxuanpham@gmail.com>
2017-06-29 20:18:43 -0700

    layout stuff

--- app/components/children/Game.js
+++ app/components/children/Game.js
@@ -7,20 +7,33 @@ var Game = React.createClass({
 
 			<div className="container">
 				{/* navbar */}
-					{/* logo far left */}
+				<nav className="navbar navbar-light bg-faded">
+  					<a class="navbar-brand" href="#">
+  						<img src=""/>
+					</a>
 					{/* menu button far right */}
-						{/* leave game (lobby) */}
-						{/* char sheet */}
-						{/* logout */}
+					<ul class="navbar-right">
+					  <div class="dropdown">
+					    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">>
+					      <img src="" width="30" height="30"/>
+					    </button>
+					    <ul class="dropdown-menu">
+					      <li>Leave Game</li>
+					      <li>Character Sheet</li>
+					      <li>Logout</li>
+					    </ul>
+					  </div>
+					</ul>
+				</nav>
 
 				{/* game div */}
 					{/* map 12*/}
-					<Map {/props/} />
+					<Map {/*props*/} />
 
 					{/* chat window 6*/}
-					<Chat {/props/} />
+					<Chat {/*props*/} />
 					{/* char sheet 6*/}
-					<ChaSht {/props/} />
+					<ChaSht {/*props*/} />
 
 					{/* turn indicator 3*/}
 					<button>{/* turn prop */}</button>
